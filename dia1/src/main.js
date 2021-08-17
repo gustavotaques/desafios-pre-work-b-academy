3fc34441cb2eeae82d515cd2100f246cb313b77a
Gustavo Taques <gustavotaques@hotmail.com>
2021-08-17 11:08:44 -0300

    Ex 04 - Usei condicionais para alterar o estado do div app

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -4,3 +4,19 @@ document.querySelector('#app').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+
+const cliqueAlterarVisibilidade = document.querySelector('[data-js="app2"]')
+
+cliqueAlterarVisibilidade.addEventListener('click', (event) => {
+  event.preventDefault()
+
+  const texto = document.querySelector('[data-js="app"]')
+
+  if(texto.style.display === "none"){
+    texto.style.display = "block"
+  }else{
+    texto.style.display = "none"
+  }
+
+
+})
