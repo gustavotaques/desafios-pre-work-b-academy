a460da241ca1857aac0997e3e74855a2b0f76464
Gustavo Taques <gustavotaques@hotmail.com>
2021-08-19 20:45:10 -0300

    Ex 02

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -15,3 +15,35 @@ input.addEventListener('input', (e) => {
     }
   }).join(' ')
 })
+
+
+const form = document.querySelector('[data-js="form"]')
+const select = document.createElement('select')
+select.setAttribute('multiple', '')
+
+const divColors = document.createElement('div')
+divColors.style.display = 'flex'
+const colors = ['#48D1CC', '#6699FF', '#66FF66', '#FF6666', '#FF66FF']
+
+
+colors.forEach(color => {
+  const option = document.createElement('option')
+  option.value = color
+  option.textContent = color
+  select.appendChild(option)
+})
+
+select.addEventListener('change', (e) => {
+  divColors.innerHTML = '',
+  [...e.target.selectedOptions].map(color => {
+    const div = document.createElement('div')
+    div.style.width = '100px'
+    div.style.height = '100px'
+    div.style.background = color.value
+
+    divColors.appendChild(div)
+  })
+})
+
+form.appendChild(select)
+document.body.appendChild(divColors)
