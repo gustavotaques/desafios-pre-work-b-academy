1470f7abb7c5cc773ef2205e1e3e2b5f9280e62d
Gustavo Taques <gustavotaques@hotmail.com>
2021-08-20 14:20:07 -0300

    dia4 - Ex 02

--- dia4/app/main.js
+++ dia4/app/main.js
@@ -1,1 +1,17 @@
 import './style.css'
+
+// GET
+const form = document.querySelector('[data-js="data-table"]')
+fetch('http://localhost:3333/cars')
+  .then(result => result.json())
+  .then(result => {
+    haveCar(result)
+  })
+
+function haveCar(result) {
+  if(result.length === 0){
+    const tr = document.createElement('tr')
+    tr.textContent = "Nenhum carro encontrado"
+    form.appendChild(tr)
+  }
+}
