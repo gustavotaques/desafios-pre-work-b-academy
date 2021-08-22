0aeef9ec0282ade69286d959d42e4239a3477751
Gustavo Taques <gustavotaques@hotmail.com>
2021-08-21 21:22:47 -0300

    dia3 - Ex 01: refactoring

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -8,8 +8,8 @@ const notCapitalize = ["da", "de", "do", "dos"]
 input.addEventListener('input', (e) => {
   const text = e.target.value.toLowerCase().split(" ")
   e.target.value = text.map((word) => {
-    if(notCapitalize.includes(word.toLowerCase())){
-      return word.toLowerCase()
+    if(notCapitalize.includes(word)){
+      return word
     }else{
       return capitalize(word)
     }
